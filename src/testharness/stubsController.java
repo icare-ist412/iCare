957e75e4b6c3b0fc51683b8e51d780f8bb4a3a7e
DMikhailov <DMikhailov@192.168.0.19>
2019-06-23 21:42:00 +0300

    added tests for Address and Appointment

--- src/testharness/stubsController.java
+++ src/testharness/stubsController.java
@@ -18,7 +18,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
-
 /**
  *
  * @author Group One
@@ -27,8 +26,7 @@ public class stubsController {
 
     private static final String HEADER = "------------------------------\nClass stub: ";
     private static final String FOOTER = "------------------------------\n";
-    
-    
+
     /**
      * @param args the command line arguments
      */
@@ -52,18 +50,17 @@ public class stubsController {
     private static void print(Object text) {
         System.out.println(text);
     }
-    
-    private static void test(String testName, Object item, Object shouldEqual){
-        
+
+    private static void test(String testName, Object item, Object shouldEqual) {
+
         //uncomment for debugging:
         //print(item + " == " +shouldEqual);
-        
-        if(item.equals(shouldEqual)){
+        if (item.equals(shouldEqual)) {
             print(testName + ": passed");
         } else {
             print(testName + ": failed");
         }
-        
+
     }
 
     private static void patientTest() {
@@ -74,11 +71,10 @@ public class stubsController {
         print(patient.getInsuranceID());
         patient.setInsuranceID(987654321);
         print(patient.getInsuranceID());
-        
+
         test("Patient.getRoleType test", patient.getRoleType(), "Patient");
         test("Patient.getInsurance test", patient.getInsuranceID(), new Long(987654321));
         test("Patient.getFullName test", patient.getFullName(), "David Ortiz");
-
 
         print(FOOTER);
     }
@@ -92,7 +88,7 @@ public class stubsController {
 
         test("Staff.getRoleType test", staff.getRoleType(), "Staff");
         test("Staff.getDepartment test", staff.getDepartment(), "IT");
-            
+
         print(FOOTER);
     }
 
@@ -115,7 +111,7 @@ public class stubsController {
         print(user.getUserID());
 
         print(user.authenticate(user.getUserID(), "test"));
-        
+
         test("User.getRoleType test", user.getRoleType(), "User");
         test("User.getBirthdate test", user.getBirthdate(), LocalDate.parse("1995-08-03", DateTimeFormatter.ofPattern("yyyy-MM-dd")));
         test("User.getFullName test", user.getFullName(), "David Ortiz");
@@ -128,33 +124,46 @@ public class stubsController {
 
     private static void addressTest() {
         print(HEADER + "Adress");
-        Address address = new Address("767 5th Ave","New York", "NY", 10153);  
+        Address address = new Address("767 5th Ave", "New York", "NY", 10153);
         print(address.getStreetAddress());
         print(address.getCity());
         print(address.getState());
         print(address.getZipCode());
+
+        test("Address.getStreetAddress test", address.getStreetAddress(), "767 5th Ave");
+        test("Address.getCity test", address.getCity(), "New York");
+        test("Address.getState test", address.getState(), "NY");
+        test("Address.getZipCode test", address.getZipCode(), 10153);
+
         print(FOOTER);
     }
 
     private static void appointmentTest() {
         print(HEADER + "Appointment");
         Appointment appointment = new Appointment();
-        Patient patient = new Patient("David", "Ortiz", 123456789, "1995-08-03");
-        Staff staff = new Staff("David", "Ortiz", "IT", "1995-08-03");
+        Patient aPatient = new Patient("David", "Ortiz", 123456789, "1995-08-03");
+        Staff employee = new Staff("David", "Ortiz", "IT", "1995-08-03");
         LocalDateTime aDate = LocalDateTime.now().plusDays(3).withHour(15).withMinute(15);
-        appointment.setAddress(new Address("767 5th Ave","New York", "NY", 10153));
+        Address anAddress = new Address("767 5th Ave", "New York", "NY", 10153);
+        appointment.setAddress(anAddress);
         appointment.setDate(aDate);
-        appointment.setPatient(patient);
-        appointment.setStaff(staff);           
+        appointment.setPatient(aPatient);
+        appointment.setStaff(employee);
         print(appointment.getAddress().toString());
         print(appointment.getDateAsString());
         print(appointment.getPatient().toString());
         print(appointment.getStaff().toString());
+
+        test("Appointment.getAddress() test", appointment.getAddress(), anAddress);
+        test("Appointment.getDateAsString() test", appointment.getDate(), aDate);
+        test("Appointment.getPatient() test", appointment.getPatient(), aPatient);
+        test("Appointment.getStaff() test", appointment.getStaff(), employee);
+
         print(FOOTER);
-        
+
     }
 
-    private static void billTest(){
+    private static void billTest() {
         print(HEADER + "Bill");
         Bill testBill = new Bill();
         testBill.setBillID(123456);
@@ -163,14 +172,14 @@ public class stubsController {
         print(testBill.getBillAmount());
         print(testBill.getBillPatientID());
         print(testBill.getBillPostedDate());
-        
+
         print("Second test");
         Bill testBill2 = new Bill();
         testBill2.setBillPostedDate(LocalDate.now());
         testBill2.setBillpatientID("123457-774");
         testBill2.setBillAmount(150);
         testBill2.setBillID(85412575);
-        
+
         print(testBill.getBillID());
         print(testBill.getBillAmount());
         print(testBill.getBillPatientID());
@@ -180,64 +189,63 @@ public class stubsController {
 
     private static void hospitalTest() {
         print(HEADER + "Hospital");
-        
+
         Hospital hospital = new Hospital();
         hospital.setAddress("500 University DR", "Hershey", "PA", 17033);
         ArrayList<Staff> staffList = new ArrayList<>();
         staffList.add(new Staff("Jake", "Benedick", "Oncology", "1900-01-01"));
         hospital.setStaffList(staffList);
-        
+
         print(hospital.getAddress().toString());
         print("StaffList: " + hospital.getStaffList().toString());
-        
+
         print(FOOTER);
     }
 
     private static void storageTest() {
         print(HEADER + "Storage");
-        
+
         try {
             Storage storage = new Storage();
         } catch (FileNotFoundException ex) {
             print("Failed to read from file");
         }
-        
+
         print(FOOTER);
     }
 
     private static void treatmentTest() {
         print(HEADER + "Treatment");
-        
+
         Treatment treatment = new Treatment("Instructions", "Medication", 6);
         print(treatment.getInstructions());
         print(treatment.getMedication());
         print(treatment.getNumberOfWeeks());
-        
+
         print(FOOTER);
     }
 
     private static void mainMenuViewControllerTest() {
-       print(HEADER + "Main Menu View Controller");
-       
-       MainMenuViewController mainMenuViewController = new MainMenuViewController();
-       User user = new User("David", "Ortiz", "1995-08-03");       
-       
-       // the other methods of MainMenuViewController pertain to GUI and can't be test using this approach for testing
-       
-       print("Successfully initialized mainMenuViewController with user information");
-       print(FOOTER);
+        print(HEADER + "Main Menu View Controller");
+
+        MainMenuViewController mainMenuViewController = new MainMenuViewController();
+        User user = new User("David", "Ortiz", "1995-08-03");
+
+        // the other methods of MainMenuViewController pertain to GUI and can't be test using this approach for testing
+        print("Successfully initialized mainMenuViewController with user information");
+        print(FOOTER);
     }
 
-    private static void InsuranceTest(){
+    private static void InsuranceTest() {
         print(HEADER + "Insurance");
-        
+
         Insurance testInsurance = new Insurance();
         testInsurance.setCompanyName("Allsafe");
         testInsurance.setCompanyPhone("800-321-1234");
         testInsurance.setCompanyAddress("12 Healthy Way, HealthyCity, MD, 29001");
         testInsurance.setPolicyNumber("4569874-869");
         testInsurance.setDeductible(500);
-        
+
         print(testInsurance.getCompanyName());
         print(testInsurance.getCompanyAddress());
         print(testInsurance.getCompanyPhone());
@@ -245,13 +253,13 @@ public class stubsController {
         print(testInsurance.getDeductible());
         print(FOOTER);
     }
-    
-    private static void immunizationTest(){
+
+    private static void immunizationTest() {
         print(HEADER + "Immunization");
-        Immunization testImmunization = new Immunization("FLU SHOT","FS");
+        Immunization testImmunization = new Immunization("FLU SHOT", "FS");
         testImmunization.setDateAdministered(LocalDate.now());
         testImmunization.setIsFollowUpRequired(false);
-        
+
         print(testImmunization.getDateAdministered());
         print(testImmunization.getImmunization());
         print(testImmunization.getImmunizationAbbreviation());
