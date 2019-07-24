34b4890a62596a225b3951cbfc57aa3646d8f212
David <42614246+deo5083@users.noreply.github.com>
2019-07-24 15:09:42 -0400

    merge branches 2 and 3

--- src/icare/models/Treatment.java
+++ src/icare/models/Treatment.java
@@ -5,9 +5,9 @@ package icare.models;
  * @author jakebenedick
  */
 public class Treatment {
-    private String instructions;
-    private String medication;
-    private int numberOfWeeks;
+    String instructions;
+    String medication;
+    int numberOfWeeks;
 
     /**
     * Treatment constructor with instructions, medication, and numberOfWeeks parameters
